76c95c77345c42042edd8b158a2859e695b9e4c8
Saincurin Peterson Denis <petersondenisso24@gmail.com>
2018-08-21 18:10:03 -0400

    Retrieve data on firebase

--- app/src/main/java/com/example/saincurin/htfacile/ModelData/DataModel.java
+++ app/src/main/java/com/example/saincurin/htfacile/ModelData/DataModel.java
@@ -1,4 +1,50 @@
 package com.example.saincurin.htfacile.ModelData;
 
 public class DataModel {
+
+    String title, image, description, quantity, price; // these must be match with the same in the database
+
+    public DataModel() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
--- app/src/main/java/com/example/saincurin/htfacile/fragments/ProductsFragment.java
+++ app/src/main/java/com/example/saincurin/htfacile/fragments/ProductsFragment.java
@@ -3,16 +3,28 @@ package com.example.saincurin.htfacile.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.saincurin.htfacile.Adapters.viewHolder;
+import com.example.saincurin.htfacile.ModelData.DataModel;
 import com.example.saincurin.htfacile.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProductsFragment extends Fragment {
+
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mReference;
+    RecyclerView mRecycleView;
+    View v;
 
 
     public ProductsFragment() {
@@ -24,7 +36,33 @@ public class ProductsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_products, container, false);
+        v = inflater.inflate(R.layout.fragment_products, container, false);
+        //RecycleView
+        mRecycleView = v.findViewById(R.id.recycleView);
+        mRecycleView.setHasFixedSize(true);
+
+        //send Query FirebaseDatabase
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mReference = mFirebaseDatabase.getReference("shop");
+
+        FirebaseRecyclerAdapter<DataModel, viewHolder> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<DataModel, viewHolder>(
+                        DataModel.class,
+                        R.layout.item_row,
+                        viewHolder.class,
+                        mReference
+                ) {
+                    @Override
+                    protected void populateViewHolder(viewHolder viewHolder, DataModel model, int position) {
+                        viewHolder.setData(getContext(), model.getTitle(), model.getDescription(), model.getImage(), model.getPrice());
+                    }
+                };
+
+                //set the adapter to the recyclerview
+                mRecycleView.setAdapter(firebaseRecyclerAdapter);
+
+
+        return v;
     }
 
 }
